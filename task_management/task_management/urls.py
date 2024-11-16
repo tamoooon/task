5cf662c2ba07d5deaa926a631ea1fd868e262e2e
tamoooon99 <zawazawa1999@gmail.com>
2024-11-16 14:38:39 +0900

    loginのURL変えたよ

--- task_management/task_management/urls.py
+++ task_management/task_management/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('task/', include('task.urls')),
+    path('', lambda request: redirect('task/login/')),  # ここでリダイレクトを設定
 ]
